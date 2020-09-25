0ffb61ddad142541e980887105409982ba8ec713
sammyrano <58597701+sammyrano@users.noreply.github.com>
2020-09-25 05:08:08 +0100

    Add files via upload

---
+++ TASK 3.py
@@ -0,0 +1,22 @@
+
+    
+sum=0
+
+def testArray(a):
+    for x in a:
+        sum=sum+1
+
+    print(sum)
+    
+    #if len(inp1)> 0 :
+        #inp1.sort()
+
+    #ran =range(len(inp1))
+    #print(ran)
+               
+    #print(len(inp1))
+    #inp1.extend(inp2)
+    
+
+
+testArray([-1,0,4,2,5,6,-7,-9])
---
+++ task 5.py
@@ -0,0 +1,10 @@
+def sum(a):
+    z= len(a)
+    while z > 0:
+        sum = q.append('a[0]')
+        print(sum)
+        z = z - 1
+    print('done')
+
+
+sum([2,5,7,3])
